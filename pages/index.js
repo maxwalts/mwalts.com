a3f31e7b9ebb0921038e4006a77bf5c94e3b9f88
Max Walts <mwalts@umich.edu>
2023-07-18 22:52:25 -0500

    add call component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { InlineWidget } from "react-calendly";
 import styles from '../styles/Home.module.css'
 import { FaLinkedin } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
@@ -8,7 +9,7 @@ import { FaGithub } from 'react-icons/fa';
 export default function Home() {
 
   return (
-    <div className={styles.container}>
+    <div className={styles.homePage}>
       <Head>
         <title>Max Walts - Homepage</title>
         <link rel="icon" href="/favicon.ico" />
@@ -16,8 +17,16 @@ export default function Home() {
 
       <main className={styles.mainnounderline}>
         <h1 className={styles.title}>
-          Under construction :)
+          Book a call
         </h1>
+
+
+        <InlineWidget
+          styles={{
+            height: '1000px',
+            width: '200%',
+          }}
+          url="https://calendly.com/max-walts/consulting-call" />
 
         <div className={styles.grid}>
 
